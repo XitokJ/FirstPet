518e9e0d5fab877ba91546302bbb58090c7d3bb2
5taZ <stasyfreak@gmail.com>
2025-11-11 02:14:51 +0300

    Revert "Наебашил хуевый метод"
    This reverts commit 40320bdd17e8cc581082ee20da5a6b283d4b837b.

--- src/service/TicketService.java
+++ src/service/TicketService.java
@@ -1,18 +1,4 @@
 package service;
 
-import model.Ticket;
-import util.IdGenerator;
-
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class TicketService {
-    private ArrayList<Ticket> tickets = new ArrayList<>();
-
-    public void bookTicket(Ticket ticket, Scanner scanner)
-    {
-        ticket.setId(IdGenerator.nextId());
-        System.out.println("Введите название");
-
-    }
 }
